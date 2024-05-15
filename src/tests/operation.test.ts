6bee330ec8e746b344bf5a4d9b4bbff83b4b5992
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-05-15 23:01:53 +0000

    test: use cases works

--- src/tests/operation.test.ts
+++ src/tests/operation.test.ts
@@ -1,6 +1,9 @@
 import { describe, it, expect } from 'vitest';
-import { calcLoss, calcNewAverageBuyCost, calcOperation, calcProfit, calcRealProfit, calcTax, calcTaxSell, checkStatus, getCurrentLoss, getQtdeBuyTransactions, hasAbleToCalcAverageBuy, updateBalance, updateLoss } from '../services/operationService';
+import { calcLoss, calcNewAverageBuyCost, calcOperation, calcProfit, calcRealProfit, calcTax, calcTaxSell, checkStatus, getCurrentLoss, getQtdeBuyTransactions, getTaxesStdOut, hasAbleToCalcAverageBuy, updateBalance, updateLoss } from '../services/operationService';
 import { IOperation } from '../types';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { parseStdin } from '../utils/parseStdin';
 
 
 
@@ -49,17 +52,10 @@ describe('Operation Services', () => {
   });
 
   it('getQtdeBuyTransactions() - qtde buy operations so far.', () => {
-    const arr: IOperation[] = [
-                      {"operation":"buy", "unit-cost":10.00, "quantity": 10000},
-                      {"operation":"sell", "unit-cost":2.00, "quantity": 5000},
-                      {"operation":"sell", "unit-cost":20.00, "quantity": 2000},
-                      {"operation":"sell", "unit-cost":20.00, "quantity": 2000},
-                      {"operation":"sell", "unit-cost":25.00, "quantity": 1000},
-                      {"operation":"buy", "unit-cost":20.00, "quantity": 10000},
-                      {"operation":"sell", "unit-cost":15.00, "quantity": 5000},
-                      {"operation":"sell", "unit-cost":30.00, "quantity": 4350},
-                      {"operation":"sell", "unit-cost":30.00, "quantity": 650}
-                    ]
+
+    // use case 7
+    const stdin_case_7 = readFileSync(resolve('./src/operations/case7.json'), 'utf8');
+    const arr: IOperation[] = parseStdin(stdin_case_7)
 
     // count 1
     expect(getQtdeBuyTransactions(arr,0)).toBe(1);
@@ -68,7 +64,7 @@ describe('Operation Services', () => {
     expect(getQtdeBuyTransactions(arr,3)).toBe(1);
     expect(getQtdeBuyTransactions(arr,4)).toBe(1);
 
-    // count 2
+    // // count 2
     expect(getQtdeBuyTransactions(arr,5)).toBe(2);
     expect(getQtdeBuyTransactions(arr,6)).toBe(2);
     expect(getQtdeBuyTransactions(arr,7)).toBe(2);
@@ -106,37 +102,72 @@ describe('Operation Services', () => {
     expect(updateBalance(0,10000,'buy')).toBe(10000); 
     expect(updateBalance(10000,5000,'sell')).toBe(5000); 
   });
+})
 
+
+
+
+describe('Use cases test', () => {
+
+  it('getTaxesStdOut() - overall stdout.', () => {
+
+    // use case 1
+    const stdin_case_1 = readFileSync(resolve('./src/operations/case1.json'), 'utf8');
+    const operations_arr_case_1 = parseStdin(stdin_case_1)
+
+    expect(getTaxesStdOut(operations_arr_case_1)).toStrictEqual([{"tax": 0},{"tax": 0},{"tax": 0}]); 
+
+
+    // use case 2
+    const stdin_case_2 = readFileSync(resolve('./src/operations/case2.json'), 'utf8');
+    const operations_arr_case_2 = parseStdin(stdin_case_2)
+    
+    expect(getTaxesStdOut(operations_arr_case_2)).toStrictEqual([{"tax": 0.00},{"tax": 10000.00},{"tax": 0.00}]); 
+
+
+    // use case 3
+    const stdin_case_3 = readFileSync(resolve('./src/operations/case3.json'), 'utf8');
+    const operations_arr_case_3 = parseStdin(stdin_case_3)
+    
+    expect(getTaxesStdOut(operations_arr_case_3)).toStrictEqual([{"tax": 0.00},{"tax": 0.00},{"tax": 1000.00}]); 
+
+
+    // use case 4
+    const stdin_case_4 = readFileSync(resolve('./src/operations/case4.json'), 'utf8');
+    const operations_arr_case_4 = parseStdin(stdin_case_4)
+    
+    expect(getTaxesStdOut(operations_arr_case_4)).toStrictEqual([{"tax": 0},{"tax": 0},{"tax": 0}]); 
+
+
+    // use case 5
+    const stdin_case_5 = readFileSync(resolve('./src/operations/case5.json'), 'utf8');
+    const operations_arr_case_5 = parseStdin(stdin_case_5)
+    
+    expect(getTaxesStdOut(operations_arr_case_5)).toStrictEqual([{"tax": 0.00},{"tax": 0.00},{"tax": 0.00},{"tax": 10000.00}]); 
+
+
+    // use case 6
+    const stdin_case_6 = readFileSync(resolve('./src/operations/case6.json'), 'utf8');
+    const operations_arr_case_6 = parseStdin(stdin_case_6)
+    
+    expect(getTaxesStdOut(operations_arr_case_6)).toStrictEqual([{"tax": 0.00},{"tax": 0.00},{"tax": 0.00},{"tax": 0.00},{"tax": 3000.00}]); 
+
+
+    // use case 7
+    const stdin_case_7 = readFileSync(resolve('./src/operations/case7.json'), 'utf8');
+    const operations_arr_case_7 = parseStdin(stdin_case_7)
+    
+    expect(getTaxesStdOut(operations_arr_case_7)).toStrictEqual([{"tax":0.00}, {"tax":0.00}, {"tax":0.00}, {"tax":0.00}, {"tax":3000.00},
+    {"tax":0.00}, {"tax":0.00}, {"tax":3700.00}, {"tax":0.00}]); 
+
+
+    // use case 8
+    const stdin_case_8 = readFileSync(resolve('./src/operations/case8.json'), 'utf8');
+    const operations_arr_case_8 = parseStdin(stdin_case_8)
+    
+    expect(getTaxesStdOut(operations_arr_case_8)).toStrictEqual([{"tax":0.00},{"tax":80000.00},{"tax":0.00},{"tax":60000.00}]); 
+  });
 
 });
 
 
-
-
-
-
-
-/**
- * 
-toBe: Utilizado para comparar valores primitivos ou verificar a identidade de um objeto. Por exemplo, verificar se um valor é exatamente igual ao esperado.
-
-toEqual: Usado para verificar a igualdade de dois objetos, arrays ou classes. Ele faz uma comparação profunda que inclui as propriedades dos objetos.
-
-toBeTruthy e toBeFalsy: Estes métodos são usados para verificar se um valor é verdadeiro ou falso no contexto booleano. Por exemplo, null, undefined, 0, false, e '' são considerados falsos.
-
-toBeNull: Verifica se um valor é null.
-
-toBeUndefined: Verifica se um valor é undefined.
-
-toBeDefined: O oposto de toBeUndefined, verifica se um valor foi definido.
-
-toBeGreaterThan, toBeGreaterThanOrEqual, toBeLessThan, toBeLessThanOrEqual: Utilizados para realizar comparações numéricas, como verificar se um valor é maior, maior ou igual, menor ou menor ou igual a outro.
-
-toMatch: Usado para verificar se um valor corresponde a uma expressão regular.
-
-toContain: Verifica se um item está presente em um array ou se uma substring está presente em uma string.
-
-toThrow: Verifica se a execução de uma função lança um erro.
-
-toHaveBeenCalled e toHaveBeenCalledTimes: Utilizados em conjunto com mocks para verificar se funções foram chamadas e quantas vezes foram chamadas.
- */
